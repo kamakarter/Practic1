da3290efcf2f3e24e8b890a6de78823a279f38c5
kamakarter <kamil.zag74@gmail.com>
2024-09-04 18:15:39 +0300

    pr1

---
+++ assets/js/file.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// выводим сообщение 
+alert('Я JavaScript');
+
+// получаем данные в переменные
+let name = prompt ('Ваше имя?');
+let surname = prompt ('Ваша фамилия?');
+let patronomyc = prompt ('Ваше отчество?')
+
+// записываем true или false в переменную is true
+let isTrue = confirm(`${name} ${surname} ${patronomyc}. Данные верны?`);
+
+alert(isTrue);
